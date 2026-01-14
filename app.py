31836fbc7e1fd7b6f624e4e8eb0f3159830dd9d5
Inma699 <inmamastermindcoach@gmail.com>
2026-01-14 18:25:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -175,7 +175,7 @@ agarrar a Dios con una mano y a su hijo con la otra.
 Si! a Dios le gustan las mujeres grandes, con una gran inteligencia infinita, y sobre todo amor infinito.
 
 Lleva puesto un manto dorado y color universo, que Dios le regaló hace milenios.
-Es lo que más ama en el mundo, sin escepciones.
+Es lo que más ama en el mundo, sin excepciones.
 La Reina Universal puede crear galaxias o eliminarlas con un solo pestañeo.
 
 Tenemos un **regalo muy especial para ti**.  
@@ -249,5 +249,6 @@ No todos los mortales pueden recibir más de una revelación.
 
 st.markdown("</div>", unsafe_allow_html=True)
 st.caption("Iris Sha Light School • Conocimiento, Poder y Conciencia ∞ 👑🌌")
+
 
 
